b1513dd7bdd5a1d3f5a4ddd63bbe728997bb8312
luisescx <luis.esculapio@live.com>
2021-10-14 19:06:40 -0300

    feat: backButton, carDetails component

---
+++ src/screens/CarDetails/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { BackButton } from "../../components/BackButton";
+import { Container, Header } from "./styles";
+
+export const CarDetails = () => {
+    return (
+        <Container>
+            <Header>
+                <BackButton />
+            </Header>
+        </Container>
+    );
+};
