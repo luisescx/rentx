cf29b8c64cf16ebd4c644324cb61cebac657299e
luisescx <luis.esculapio@Live.com>
2021-10-24 16:39:58 -0300

    feat: scheduling screen

--- src/screens/Scheduling/index.tsx
+++ src/screens/Scheduling/index.tsx
@@ -3,7 +3,20 @@ import { StatusBar } from "react-native";
 import { useTheme } from "styled-components/native";
 import { BackButton } from "../../components/BackButton";
 
-import { Container, Header, Title } from "./styles";
+import {
+    Container,
+    Header,
+    Title,
+    RentalPeriod,
+    DateInfo,
+    DateTitle,
+    DateValue,
+    Content,
+    Footer,
+} from "./styles";
+import ArrowSvg from "../../../assets/arrow.svg";
+import { Button } from "../../components/Button";
+import Calendar from "../../components/Calendar";
 
 export const Scheduling = () => {
     const theme = useTheme();
@@ -23,7 +36,29 @@ export const Scheduling = () => {
                     data de inicio e {"\n"}
                     fim do aluguel
                 </Title>
+
+                <RentalPeriod>
+                    <DateInfo>
+                        <DateTitle>DE</DateTitle>
+                        <DateValue selected={false}></DateValue>
+                    </DateInfo>
+
+                    <ArrowSvg />
+
+                    <DateInfo>
+                        <DateTitle>ATÉ</DateTitle>
+                        <DateValue selected={true}>18/06/2021</DateValue>
+                    </DateInfo>
+                </RentalPeriod>
             </Header>
+
+            <Content>
+                <Calendar />
+            </Content>
+
+            <Footer>
+                <Button title="Confirmar" />
+            </Footer>
         </Container>
     );
 };
