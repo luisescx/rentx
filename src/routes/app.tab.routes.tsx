d2d1c5b73edfc22331a4b8ffeacb3802a8db8937
luisescx <luis.esculapio@Live.com>
2021-12-09 22:57:20 -0300

    feat: add bottom tabs icons

--- src/routes/app.tab.routes.tsx
+++ src/routes/app.tab.routes.tsx
@@ -6,6 +6,9 @@ import MyCars from "../screens/MyCars";
 import { AppStackRoutes } from "./app.stack.routes";
 import { useTheme } from "styled-components";
 import { Platform } from "react-native";
+import HomeSvg from "../../assets/home.svg";
+import CarSvg from "../../assets/car.svg";
+import PeopleSvg from "../../assets/people.svg";
 
 const { Navigator, Screen } = createBottomTabNavigator<RootParamList>();
 
@@ -27,9 +30,33 @@ export function AppTabRoutes() {
                 },
             }}
         >
-            <Screen name="HomeTab" component={AppStackRoutes} />
-            <Screen name="Profile" component={Home} />
-            <Screen name="MyCars" component={MyCars} />
+            <Screen
+                name="HomeTab"
+                component={AppStackRoutes}
+                options={{
+                    tabBarIcon: ({ color }) => (
+                        <HomeSvg width={24} height={24} fill={color} />
+                    ),
+                }}
+            />
+            <Screen
+                name="Profile"
+                component={Home}
+                options={{
+                    tabBarIcon: ({ color }) => (
+                        <CarSvg width={24} height={24} fill={color} />
+                    ),
+                }}
+            />
+            <Screen
+                name="MyCars"
+                component={MyCars}
+                options={{
+                    tabBarIcon: ({ color }) => (
+                        <PeopleSvg width={24} height={24} fill={color} />
+                    ),
+                }}
+            />
         </Navigator>
     );
 }
