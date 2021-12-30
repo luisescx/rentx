3308ca892aa18cfe96b452f313f8b3b6ed83368e
luisescx <luis.esculapio@Live.com>
2021-12-30 19:40:17 -0300

    add watermelon new table cars

---
+++ src/database/schema/CarSchema.ts
@@ -0,0 +1,37 @@
+import { tableSchema } from "@nozbe/watermelondb";
+
+const carSchema = tableSchema({
+    name: "cars",
+    columns: [
+        {
+            name: "name",
+            type: "string",
+        },
+        {
+            name: "brand",
+            type: "string",
+        },
+        {
+            name: "about",
+            type: "string",
+        },
+        {
+            name: "fuel_type",
+            type: "string",
+        },
+        {
+            name: "period",
+            type: "number",
+        },
+        {
+            name: "price",
+            type: "number",
+        },
+        {
+            name: "thumbnail",
+            type: "string",
+        },
+    ],
+});
+
+export { carSchema };
